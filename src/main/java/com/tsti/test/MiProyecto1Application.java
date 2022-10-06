a5968bd5d6f1d1313e0d211158f02ec4087525dd
dardokuttel <dardokuttel@gmail.com>
2022-10-06 18:44:20 -0300

    subida inicial

---
+++ src/main/java/com/tsti/test/MiProyecto1Application.java
@@ -0,0 +1,13 @@
+package com.tsti.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiProyecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiProyecto1Application.class, args);
+	}
+
+}
